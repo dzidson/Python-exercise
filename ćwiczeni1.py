0e06a73a4c7509265fc6fa83cff54249de29e732
Paweł Sierant <pawelsierant@gmail.com>
2023-02-03 18:10:15 +0100

    nowy commit

--- ćwiczeni1.py
+++ ćwiczeni1.py
@@ -2,7 +2,6 @@ x = input("Podaj swoje imię: ") #pobierz imię użytkownika
 y = int(input("Podaj swój wiek: ")) #pobierz wiek użytkownika
 z = input("Podaj swój ulubiony kolor: ") #pobierz jego ulubiony kolor
 
-
 print("Hej" + x + "Twój ulubiony kolor to:", z) 
 print("Masz", y, "lat" )
 print("Za rok będziesz mieć:", y+1, "lat")
