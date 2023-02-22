318305aea4e063cda813bd86cd31e141341c472f
Dzidson <pawelsierant@gmail.com>
2023-02-22 16:26:00 +0100

    Random-nauka

---
+++ losowosc.py
@@ -0,0 +1,21 @@
+import random
+
+def will_weapon_hit(weaponChance):              # stworzenie funckji czy broń trafi
+    IsHitChance = random.uniform(0,100)             # losowanie liczby od 0 do 100 
+    if (IsHitChance < weaponChance):
+        print('hit')
+    else:
+        print('no hit')
+
+listHit = []   #stworzenie listy w której zapisują sie wyniki 
+
+x = 0
+while x < 100:
+    x = x + 1
+    listHit.append(will_weapon_hit(50))
+
+from collections import Counter 
+dictionaryHit = Counter(listHit)
+print(listHit)
+
+
