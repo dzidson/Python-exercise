c1b3be3b80cee98687e78a88baa8c7d41e883695
Dzidson <pawelsierant@gmail.com>
2023-02-28 18:23:44 +0100

    cwiczenia\nauka

---
+++ import requests.py
@@ -0,0 +1,36 @@
+import requests
+
+def check(url):
+    try:
+        r = requests.get(url)
+        return r.status_code == 200
+    except: 
+        return False
+
+def strony(nazwa_pliku):           #funkcja do pobierania stron z pliku
+    with open(nazwa_pliku, "r") as file:
+        web = file.readlines()
+        web = [x.replace("\n", "") for x in web]
+        return web
+
+def filtrowanie(web):                #funkcja do filtrowania stron
+    list_web = []
+    for strona in web:
+        if check(strona):
+            list_web.append(strona)
+    return list_web
+
+
+def zapis(lista):      #funkcja do zapisywania stron
+    with open('result.txt',"w") as file:
+        for strona in true_websites:
+            file.write(strona + "\n")
+
+
+
+
+websites = strony('websites.txt')
+true_websites = filtrowanie(websites)
+
+print(true_websites)
+zapis(true_websites)
